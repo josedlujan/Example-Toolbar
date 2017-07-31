77267c4b0ef58ab90a2515cab0bbfb93fe0d82cb
josedlujan <josedlujan@gmail.com>
2017-07-31 14:17:41 -0500

    Initial commit

---
+++ EjemploToolbar/app/src/main/java/toolbar/ejemplo/com/ejemplotoolbar/MainActivity.java
@@ -0,0 +1,21 @@
+package toolbar.ejemplo.com.ejemplotoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.InflateException;
+import android.view.Menu;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
